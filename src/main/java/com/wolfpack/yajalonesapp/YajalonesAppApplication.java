97ef7b9fe651fe0e60999b72bdfd9c8d32988116
Cristian <CristianVazquez09@users.noreply.github.com>
2025-06-22 09:05:13 -0600

    feat : Inicio del proyecto

---
+++ src/main/java/com/wolfpack/yajalonesapp/YajalonesAppApplication.java
@@ -0,0 +1,13 @@
+package com.wolfpack.yajalonesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YajalonesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YajalonesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wolfpack/yajalonesapp/YajalonesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfpack.yajalonesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YajalonesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
