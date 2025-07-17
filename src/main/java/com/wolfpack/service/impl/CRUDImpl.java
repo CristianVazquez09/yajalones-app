b9b028d183842ef2898296a7c207e10de9783604
Cristian <CristianVazquez09@users.noreply.github.com>
2025-07-17 15:21:35 -0600

    refactor(crud): renombrar métodos en CRUDImpl
    - Se renombraron los métodos en `CRUDImpl.java` para seguir la convención de nombres
    - Mejora de la legibilidad y consistencia en la capa de servicio genérica.

--- src/main/java/com/wolfpack/service/impl/CRUDImpl.java
+++ src/main/java/com/wolfpack/service/impl/CRUDImpl.java
@@ -13,28 +13,28 @@ public abstract class CRUDImpl<T, ID> implements ICRUD<T, ID> {
     protected abstract IGenericRepo<T, ID> getRepo();
 
     @Override
-    public T save(T t) throws Exception {
+    public T guardar(T t) throws Exception {
         return getRepo().save(t);
     }
 
     @Override
-    public T update(ID id, T t) throws Exception {
+    public T actualizar(ID id, T t) throws Exception {
         getRepo().findById(id).orElseThrow(() -> new ModelNotFoundException("ID NOT FOUND: " + id));
         return getRepo().save(t);
     }
 
     @Override
-    public List<T> findAll() throws Exception {
+    public List<T> buscarTodos() throws Exception {
         return getRepo().findAll();
     }
 
     @Override
-    public T findById(ID id) throws Exception {
+    public T buscarPorId(ID id) throws Exception {
         return getRepo().findById(id).orElseThrow(()-> new ModelNotFoundException("ID NOT FOUND: " + id));
     }
 
     @Override
-    public void delete(ID id) throws Exception {
+    public void eliminar(ID id) throws Exception {
         getRepo().findById(id).orElseThrow(() -> new ModelNotFoundException("ID NOT FOUND: " + id));
         getRepo().deleteById(id);
     }
